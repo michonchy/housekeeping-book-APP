d3683ce60113e6717bd1b5c6e084daed862d1867
michonchy <mintan0222@gmail.com>
2022-06-03 18:12:20 +0900

    categoriesの追加

---
+++ sql/insert_categories.sql
@@ -0,0 +1,9 @@
+INSERT INTO categories VALUES(0, 'food');
+INSERT INTO categories VALUES(0, 'daily necessities');
+INSERT INTO categories VALUES(0, 'rent');
+INSERT INTO categories VALUES(0, 'water');
+INSERT INTO categories VALUES(0, 'gas');
+INSERT INTO categories VALUES(0, 'electricity');
+INSERT INTO categories VALUES(0, 'gasoline');
+INSERT INTO categories VALUES(0, 'leisure');
+INSERT INTO categories VALUES(0, 'saving');
