e609236348d21c60e802a4b81d6337887ed37d25
michonchy <mintan0222@gmail.com>
2022-06-09 19:36:44 +0900

    pythonからtableを参照できるようにした

--- app/app.py
+++ app/app.py
@@ -1,21 +1,24 @@
 import os
 import sys
 import sqlalchemy as sql
+
+def show_table(engine, table_name: str):
+    rows = engine.execute(f"select*from {table_name};")
+    print(rows)
+    for row in rows:
+        print(row)
+
 def execute_cli():
     # コマンドライン引数(2つ目以降を取得
     # requirements.txt
     # 実行
     # pip install -r requirements.txt
-    x=sys.argv[1:]
-    print("値",x)
-    print("型",type(x))
+    args=sys.argv[1:]
+    subcommand = args[0]
     db_dir = "../sql/house_keeping_book.sqlite"
     database_path = "sqlite:///" + os.path.abspath(db_dir)
     engine = sql.create_engine(database_path)
-    print(engine)
-    rows = engine.execute("select*from users;")
-    print(rows)
-    for row in rows:
-        print(row)
+    show_table(engine, subcommand)
+
 if __name__=='__main__':
     execute_cli()
