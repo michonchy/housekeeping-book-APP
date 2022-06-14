dc370996e10ea81c4810098c4ab165023c310bed
O6lvl4 <motodera@phismize.com>
2022-06-14 17:41:19 +0900

    DBの処理を共通化、api.pyとapp.pyをリファクタリング

--- app/api.py
+++ app/api.py
@@ -10,27 +10,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session as BaseSession
 from sqlalchemy.orm.query import Query
+from database import DBSession
 from models.user import User
 
 Base = declarative_base()
 
 app = Flask(__name__)
-
-_db: sqlalchemy.engine.Engine
-
-
-def _created_db():
-    if 'db' not in g:
-        db_dir = "../sql/house_keeping_book.sqlite"
-        database_path = "sqlite:///" + os.path.abspath(db_dir)
-        _db = sqlalchemy.create_engine(database_path)
-    return _db
-
-
-def db_session() -> BaseSession:
-    Session = sessionmaker(bind=_created_db())
-    session: BaseSession = Session()
-    return session
 
 
 @app.route('/')
@@ -39,15 +24,9 @@ def hello():
     return name
 
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
-
-
 @app.route('/users', methods=['GET'])
 def read_users():
-    session = db_session()
+    session = DBSession().issued()
     queried: Query[User] = session.query(User)
     users: List[User] = queried.all()
     ResponseData = TypedDict('UserResponse', {'id': int, 'name': str})
--- app/app.py
+++ app/app.py
@@ -1,49 +1,48 @@
 import os
 import sys
+from typing import List
 import sqlalchemy as sql
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (Table, Column, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
+from database import DBSession
 from models.user import User
 
 Base = declarative_base()
 
 
-def show_table(engine, table_name: str):
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    all_users = session.query(User).all()
-    print(all_users)
-    for user in all_users:
+def show_table(model_class_name: str):
+    session = DBSession().issued()
+    queried = session.query(globals()[model_class_name])
+    all_model: List = queried.all()
+    for model in all_model:
         print("---")
-        print(user.id)
-        print(user.name)
+        print(model.__dict__)
 
-    # rows = engine.execute(f"select*from {table_name};")
-    # rows = engine.execute(f"select*from users;")
-    # print(rows)
-    # for row in rows:
-    #     print("---")
-    #     print(row[0])
-    #     print(row[1])
+
+def sql(query: str):
+    session = DBSession().issued()
+    rows = session.execute(query)
+    print(rows)
+    for row in rows:
+        print(row)
 
 
 def execute_cli():
-    # コマンドライン引数(2つ目以降を取得
-    # requirements.txt
-    # 実行
-    # pip install -r requirements.txt
     args = sys.argv[1:]
     if len(args) == 0:
         print("Please input a parameter.")
         return
     subcommand = args[0]
-    db_dir = "../sql/house_keeping_book.sqlite"
-    database_path = "sqlite:///" + os.path.abspath(db_dir)
-    engine = sql.create_engine(database_path)
     if subcommand == "read":
         table_name = args[1]
-        show_table(engine, table_name)
+        show_table(table_name)
+    elif subcommand == "read_sql":
+        table_name = args[1]
+        sql(f'SELECT * FROM {table_name}')
+    elif subcommand == "sql":
+        query = args[1]
+        sql(query)
 
 
 if __name__ == '__main__':
