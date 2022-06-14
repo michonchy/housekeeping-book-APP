244c9d463e939ceec8749bd7ee3afd6d792654ce
O6lvl4 <motodera@phismize.com>
2022-06-14 17:10:00 +0900

    SQLite3で定義したuseresテーブルをAPI呼び出しと紐付け

--- app/api.py
+++ app/api.py
@@ -1,25 +1,36 @@
 import os
+from re import A
+import sqlite3
+from typing import List, TypedDict
+from requests import Session
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from flask import Flask
+from flask import Flask, g, jsonify
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session as BaseSession
+from sqlalchemy.orm.query import Query
 from models.user import User
 
 Base = declarative_base()
 
 app = Flask(__name__)
 
-
-def create_engine():
-    db_dir = "../sql/house_keeping_book.sqlite"
-    database_path = "sqlite:///" + os.path.abspath(db_dir)
-    engine = sqlalchemy.create_engine(database_path)
+_db: sqlalchemy.engine.Engine
 
 
-db = create_engine()
-Session = sessionmaker(bind=db)
-session = Session()
+def _created_db():
+    if 'db' not in g:
+        db_dir = "../sql/house_keeping_book.sqlite"
+        database_path = "sqlite:///" + os.path.abspath(db_dir)
+        _db = sqlalchemy.create_engine(database_path)
+    return _db
+
+
+def db_session() -> BaseSession:
+    Session = sessionmaker(bind=_created_db())
+    session: BaseSession = Session()
+    return session
 
 
 @app.route('/')
@@ -34,17 +45,24 @@ def good():
     return name
 
 
-@app.route('/users')
+@app.route('/users', methods=['GET'])
 def read_users():
-    all_users = session.query(User).all()
-    print(all_users)
-    for user in all_users:
-        print("---")
-        print(user.id)
-        print(user.name)
-    return all_users[0]
+    session = db_session()
+    queried: Query[User] = session.query(User)
+    users: List[User] = queried.all()
+    ResponseData = TypedDict('UserResponse', {'id': int, 'name': str})
+    result: List[ResponseData] = []
+    print(users)
+    for user in users:
+        result.append({
+            "id": user.id,
+            "name": user.name
+        })
+    return jsonify({
+        'code': 200,
+        'data': result
+    })
 
 
-# おまじない
 if __name__ == "__main__":
     app.run(debug=True)
