634ae189c38673e13f5ff54f47be604fcdccf629
O6lvl4 <motodera@phismize.com>
2022-06-14 16:20:52 +0900

    importの書き方を修正

--- app/api.py
+++ app/api.py
@@ -4,32 +4,35 @@ from sqlalchemy.orm import sessionmaker
 from flask import Flask
 from sqlalchemy.ext.declarative import declarative_base
 
+from models.user import User
+
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-    id = sqlalchemy.Column(sqlalchemy.Integer(),primary_key=True)
-    name = sqlalchemy.Column(sqlalchemy.String())
-
 app = Flask(__name__)
+
 
 def create_engine():
     db_dir = "../sql/house_keeping_book.sqlite"
     database_path = "sqlite:///" + os.path.abspath(db_dir)
     engine = sqlalchemy.create_engine(database_path)
+
+
 db = create_engine()
 Session = sessionmaker(bind=db)
 session = Session()
+
 
 @app.route('/')
 def hello():
     name = "Hello World"
     return name
 
+
 @app.route('/good')
 def good():
     name = "Good"
     return name
+
 
 @app.route('/users')
 def read_users():
@@ -41,6 +44,7 @@ def read_users():
         print(user.name)
     return all_users[0]
 
-## おまじない
+
+# おまじない
 if __name__ == "__main__":
     app.run(debug=True)
--- app/app.py
+++ app/app.py
@@ -4,13 +4,9 @@ import sqlalchemy as sql
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (Table, Column, Integer, String)
 from sqlalchemy.ext.declarative import declarative_base
+from models.user import User
 
 Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer(),primary_key=True)
-    name = Column(String())
 
 
 def show_table(engine, table_name: str):
@@ -31,15 +27,16 @@ def show_table(engine, table_name: str):
     #     print(row[0])
     #     print(row[1])
 
+
 def execute_cli():
     # コマンドライン引数(2つ目以降を取得
     # requirements.txt
     # 実行
     # pip install -r requirements.txt
-    args=sys.argv[1:]
+    args = sys.argv[1:]
     if len(args) == 0:
         print("Please input a parameter.")
-        return 
+        return
     subcommand = args[0]
     db_dir = "../sql/house_keeping_book.sqlite"
     database_path = "sqlite:///" + os.path.abspath(db_dir)
@@ -48,5 +45,6 @@ def execute_cli():
         table_name = args[1]
         show_table(engine, table_name)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     execute_cli()
